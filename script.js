7f0e49f85b806722ecadc7bf48fea901a921067c
Farah Faizah <l200234060@student.ums.ac.id>
2025-03-26 09:20:38 +0700

    revisi

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const hamburger = document.querySelector(".hamburger");
-    const navLinks = document.querySelector(".nav-links");
-
-    hamburger.addEventListener("click", function () {
-        navLinks.classList.toggle("active");
-    });
-});
